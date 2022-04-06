460a9c82753198cb9a8db1a55baa8c95d8a0baf9
Mehedi Hasan Tayef <tayef05@outlook.com>
2022-04-06 22:33:21 +0600

    add environment variable to secure database

--- eurolin/settings.py
+++ eurolin/settings.py
@@ -20,12 +20,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6=pq@667j%fjbyt7z2j+(qy3co@5==eyof)i%i4deabdk3^93&'
+import os
+
+import environ
+
+env = environ.Env()
+
+# read th .env file
+environ.Env.read_env()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +87,12 @@ WSGI_APPLICATION = 'eurolin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': env("DATABASE_PORT"),
     }
 }
 
